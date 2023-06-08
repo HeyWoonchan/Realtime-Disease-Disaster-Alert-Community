55b235ea4c1762144f3fe9d20c40cf0741a26cc1
ahntae98 <ammjy1234@gmail.com>
2023-06-08 22:31:55 +0900

    Css 일부 수정

--- project1/buttonupdate/buttontest.py
+++ project1/buttonupdate/buttontest.py
@@ -665,4 +665,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     create_table()
-    app.run(host='localhost', port=8000)
+    app.run(host='localhost', port=7000)
